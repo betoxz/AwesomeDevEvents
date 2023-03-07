3021ef874eadb32a3e9f3be3cad5794e49fad7ba
Carlos Alberto Martins <betoxz@gmail.com>
2023-03-07 01:17:03 -0300

    Add project files.

---
+++ AwesomeDevEvents.API/Entities/DevEventSpeaker.cs
@@ -0,0 +1,13 @@
+﻿namespace AwesomeDevEvents.API.Entities
+{
+    public class DevEventSpeaker
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string TalkTitle { get; set; }
+
+        public string TalskDescription { get; set; }
+        public string LinkedInProfile { get; set; }
+
+    }
+}
